22813ae157f4b497604b641fde91d0feca7cf479
Naufal Adriel Fauzi <fnaufaladriel@gmail.com>
2021-12-02 04:45:08 +0700

    coba styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-
 import Komponen from "./components/komponen";
 import Number from "./components/komponen/number";
 import picture from "./mgm.jpg"
-
+import './Styling/plain/plain.css'
 function App() {
   return (
-    <div className="App">
+    <div className='container'>
       <Komponen />
       <img src={picture} alt="mgm"height="100px"></img>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate magnam culpa debitis inventore, asperiores quas ullam quo recusandae, maiores dolor tempore fuga soluta eaque quod. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, eum quos. Suscipit dignissimos eos, reprehenderit sequi ipsa sed voluptatum veritatis vitae illum unde aliquid ab praesentium, at quos enim iusto et similique autem itaque ratione minus. Explicabo voluptas nulla, molestias repellat necessitatibus suscipit ea consequuntur. Iusto sed nulla tenetur fuga.</p>
-      <button id="plus">+</button>
-      <Number />
-      <button id="minus">-</button>
+      <div className="angka">
+        <button id="plus">+</button>
+        <Number />
+        <button id="minus">-</button>
+      </div>
+      
     </div>
   );
 }
--- src/components/komponen/Function.jsx
+++ src/components/komponen/Function.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Function = (props) => {
     return (
